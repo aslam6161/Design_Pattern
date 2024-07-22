07c57823c4822791c164b8fa5580bbdb3bbb243a
aslam6161 <aslam.exe1@gmail.com>
2024-07-22 10:28:26 +0800

    Observer pattern added

---
+++ ObserverPattern/IJobPostings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.ObserverPattern
+{
+    interface IJobPostings
+    {
+        void Attach(IJobSeeker jobSeeker);
+        void Detach(IJobSeeker jobSeeker);
+        void Notify(Job job);
+
+    }
+}
---
+++ ObserverPattern/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.ObserverPattern
+{
+    class Job
+    {
+        public string Description { get; set; }
+        public Job(string descriptioin)
+        {
+            this.Description = descriptioin;
+        }
+    }
+}
